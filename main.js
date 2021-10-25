68753ac5791d94990412e725c77b3c98804180b5
Diogo0602x <diogoteixeira0602@gmail.com>
2021-10-25 12:42:19 -0300

    Aula 4 terminada

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'Diogo0602x',
+  youtube: 'jakelinygracielly',
+  facebook: 'diogo0602',
+  instagram: 'diogosalest',
+  twitter: 'jakelinytec'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
